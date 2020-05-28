a15ded2468e4bfd5a461fdf97c8a5f082bb8cb59
AvatarGanymede <liu.ruitong@outlook.com>
2020-05-29 00:03:38 +0800

    Update record_manager.h

--- Code/record_manager.h
+++ Code/record_manager.h
@@ -10,6 +10,9 @@ private:
 	char data[BLOCKSIZE];
 	std::string filename;
 	int block_id;
+	bool dirty;
+	bool locked;
+	bool ref;
 public:
 	Block(std::string table = "", int _block_id = -1) {
 		filename = RECORDPATH + table + ".data";
@@ -19,7 +22,14 @@ public:
 		}
 		data[1] = sprintf(data + 1, "%08x", BLOCKSIZE - 1);
 	}
-	void InsertRecord();
+	int GetBlockID();
+	void setDirty(bool _dirty);
+	bool isDirrty();
+	void setLocked(bool _locked);
+	bool isLocked();
+	void setRef(bool _ref);
+	bool getRef();
+	char* getData();
 };
 
 #endif
