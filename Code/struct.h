29eba054a528b5ae35573c3265c8db84d010ef21
AvatarGanymede <liu.ruitong@outlook.com>
2020-05-28 23:35:05 +0800

    结构体

---
+++ Code/struct.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef _BASIC_H_
+#define _BASIC_H_ 1
+
+#include "pch.h"
+
+struct Attribute {
+    int attri_num;  
+    std::string attri_name[32]; 
+    short attri_type[32]; 
+    bool is_unique[32];  //判断每个属性是否unique，是为true
+    short primary_key;  //判断是否存在主键,-1为不存在，其他则为主键的所在位置
+    bool has_index[32]; //判断是否存在索引
+};
+
+#endif
