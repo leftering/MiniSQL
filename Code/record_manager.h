419d8ecb3a7c9bc0bdcb62496f1772807e8ac61b
AvatarGanymede <liu.ruitong@outlook.com>
2020-05-28 23:35:11 +0800

    Update record_manager.h

--- Code/record_manager.h
+++ Code/record_manager.h
@@ -2,7 +2,7 @@
 #define _RECORD_MANAGER_H_ 
 
 #include "pch.h"
-#include "config.h"
+#include "define.h"
 
 class Block
 {
@@ -10,10 +10,16 @@ private:
 	char data[BLOCKSIZE];
 	std::string filename;
 	int block_id;
-	bool dirty;
-	bool locked;
 public:
-
+	Block(std::string table = "", int _block_id = -1) {
+		filename = RECORDPATH + table + ".data";
+		block_id = _block_id;
+		for (int i = 0; i < BLOCKSIZE; i++) {
+			data[i] = 0;
+		}
+		data[1] = sprintf(data + 1, "%08x", BLOCKSIZE - 1);
+	}
+	void InsertRecord();
 };
 
 #endif
