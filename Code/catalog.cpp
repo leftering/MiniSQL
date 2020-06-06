a4cb54be2aa6009112103e6bc3d7822cfa9b922a
xxx <1104132275@qq.com>
2020-06-06 13:25:14 +0800

    debug

--- Code/catalog.cpp
+++ Code/catalog.cpp
@@ -24,25 +24,36 @@ bool table_info::write_table_info() {
 }
 
 bool table_info::get_table_info(string table_name) {
-  ifstream fin(table_name);
-  cout << table_name << endl;
-  if (!fin) {
+ // ifstream fin(table_name);
+ // if (!fin) {
+	//return false;
+ // }
+ // else {
+	//fin >> this->table_name;
+	//fin >> this->col_num;
+	//for (int i = 0;i < this->col_num;i++) {
+	//  fin >> this->col[i].col_name;
+	//  int type;
+	//  fin >> type;
+	//  this->col[i].col_type = (colunm_type)type;
+	//  fin >> this->col[i].char_length;
+	//  fin >> this->col[i].have_index;
+	//  fin >> this->col[i].primary_key;
+	//  fin >> this->col[i].unique;
+	//}
+ // }
+ // fin.close();
+ // fin.clear();
+ // return true;
+  FILE *fp = NULL;
+  fp = fopen(table_name.c_str(), "r");
+  if (!fp) {
 	return false;
   }
-  else {
-	fin >> this->table_name;
-	fin >> this->col_num;
-	for (int i = 0;i < this->col_num;i++) {
-	  fin >> this->col[i].col_name;
-	  int type;
-	  fin >> type;
-	  this->col[i].col_type = (colunm_type)type;
-	  fin >> this->col[i].char_length;
-	  fin >> this->col[i].have_index;
-	  fin >> this->col[i].primary_key;
-	  fin >> this->col[i].unique;
-	}
+  fscanf(fp, "%s%d", this->table_name, &(this->col_num));
+  for (int i = 0;i < this->col_num;i++) {
+	fscanf(fp, "%s%d%d%d%d%d", this->col[i].col_name, &(this->col[i].col_type), &(this->col[i].char_length), &(this->col[i].have_index), &(this->col[i].primary_key), &(this->col[i].unique));
   }
-  fin.close();
+  fclose(fp);
   return true;
 }
