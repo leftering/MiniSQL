233c120486ba4bf94fd3756ee35fb26604d9b433
xxx <1104132275@qq.com>
2020-06-06 13:52:36 +0800

    deb

--- Code/catalog.cpp
+++ Code/catalog.cpp
@@ -24,36 +24,46 @@ bool table_info::write_table_info() {
 }
 
 bool table_info::get_table_info(string table_name) {
- // ifstream fin(table_name);
- // if (!fin) {
-	//return false;
- // }
- // else {
-	//fin >> this->table_name;
-	//fin >> this->col_num;
-	//for (int i = 0;i < this->col_num;i++) {
-	//  fin >> this->col[i].col_name;
-	//  int type;
-	//  fin >> type;
-	//  this->col[i].col_type = (colunm_type)type;
-	//  fin >> this->col[i].char_length;
-	//  fin >> this->col[i].have_index;
-	//  fin >> this->col[i].primary_key;
-	//  fin >> this->col[i].unique;
-	//}
- // }
- // fin.close();
- // fin.clear();
- // return true;
-  FILE *fp = NULL;
-  fp = fopen(table_name.c_str(), "r");
-  if (!fp) {
+  static ifstream fin;
+  fin.open(table_name);
+  if (!fin) {
 	return false;
   }
-  fscanf(fp, "%s%d", this->table_name, &(this->col_num));
-  for (int i = 0;i < this->col_num;i++) {
-	fscanf(fp, "%s%d%d%d%d%d", this->col[i].col_name, &(this->col[i].col_type), &(this->col[i].char_length), &(this->col[i].have_index), &(this->col[i].primary_key), &(this->col[i].unique));
+  else {
+	fin.seekg(0, ios::beg);
+	fin >> this->table_name;
+	fin >> this->col_num;
+	for (int i = 0;i < this->col_num;i++) {
+	  fin >> this->col[i].col_name;
+	  int type;
+	  fin >> type;
+	  this->col[i].col_type = (colunm_type)type;
+	  fin >> this->col[i].char_length;
+	  fin >> this->col[i].have_index;
+	  fin >> this->col[i].primary_key;
+	  fin >> this->col[i].unique;
+	}
   }
-  fclose(fp);
+  fin.close();
+  fin.clear();
   return true;
+
+
+ // static FILE *fp = NULL;
+ // static string fname = "";
+ // if (fname != table_name)
+	//fp = fopen(table_name.c_str(), "r");
+ // fname = table_name;
+ // if (!fp) {
+	//cout << "Error: " << strerror(errno);
+	//system("pause");
+	//return false;
+ // }
+ // fseek(fp, 0, SEEK_SET);
+ // fscanf(fp, "%s%d", this->table_name, &(this->col_num));
+ // for (int i = 0;i < this->col_num;i++) {
+	//fscanf(fp, "%s%d%d%d%d%d", this->col[i].col_name, &(this->col[i].col_type), &(this->col[i].char_length), &(this->col[i].have_index), &(this->col[i].primary_key), &(this->col[i].unique));
+ // }
+ // fclose(fp);
+ // return true;
 }
